17ebbbcb3d4c2ad8f09293a3e5e44fa9849ceefe
Max Rausch-Dupont <maxrd79@gmail.com>
2021-12-01 10:32:48 +0100

    Fix typing

--- src/adventofcode/year_2021/day_01_2021.py
+++ src/adventofcode/year_2021/day_01_2021.py
@@ -1,17 +1,22 @@
-from typing import List
+from typing import List, cast
 
 from adventofcode.util.exceptions import SolutionNotFoundException
 from adventofcode.util.helpers import solution_timer
 from adventofcode.util.input_helpers import get_input_for_day
 
 
+def sonar_sweep(values: List[int]) -> int:
+    increasing = 0
+    for previous, current in zip(values[:-1], values[1:]):
+        if previous < current:
+            increasing += 1
+    return increasing
+
+
 @solution_timer(2021, 1, 1)
 def part_one(input_data: List[str]):
-    input_data = list(map(int, input_data))
-    answer = 0
-    for previous, current in zip(input_data[:-1], input_data[1:]):
-        if previous < current:
-            answer += 1
+    sonar_values = cast(List[int], list(map(int, input_data)))
+    answer = sonar_sweep(sonar_values)
 
     if not answer:
         raise SolutionNotFoundException(2021, 1, 1)
@@ -21,13 +26,13 @@ def part_one(input_data: List[str]):
 
 @solution_timer(2021, 1, 2)
 def part_two(input_data: List[str]):
-    input_data = list(map(int, input_data))
+    sonar_values = cast(List[int], list(map(int, input_data)))
 
     windows = []
-    for window in zip(input_data[:-1], input_data[1:], input_data[2:]):
+    for window in zip(sonar_values[:-1], sonar_values[1:], sonar_values[2:]):
         windows.append(sum(window))
 
-    answer = part_one(windows)
+    answer = sonar_sweep(windows)
 
     if not answer:
         raise SolutionNotFoundException(2021, 1, 2)
