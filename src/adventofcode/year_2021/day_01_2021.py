082e9ae81971c50419314f0dc080fdb52d5c2773
Max Rausch-Dupont <maxrd79@gmail.com>
2021-12-01 10:20:13 +0100

    Add day_1

--- src/adventofcode/year_2021/day_01_2021.py
+++ src/adventofcode/year_2021/day_01_2021.py
@@ -7,7 +7,11 @@ from adventofcode.util.input_helpers import get_input_for_day
 
 @solution_timer(2021, 1, 1)
 def part_one(input_data: List[str]):
-    answer = ...
+    input_data = list(map(int, input_data))
+    answer = 0
+    for previous, current in zip(input_data[:-1], input_data[1:]):
+        if previous < current:
+            answer += 1
 
     if not answer:
         raise SolutionNotFoundException(2021, 1, 1)
@@ -17,7 +21,13 @@ def part_one(input_data: List[str]):
 
 @solution_timer(2021, 1, 2)
 def part_two(input_data: List[str]):
-    answer = ...
+    input_data = list(map(int, input_data))
+
+    windows = []
+    for window in zip(input_data[:-1], input_data[1:], input_data[2:]):
+        windows.append(sum(window))
+
+    answer = part_one(windows)
 
     if not answer:
         raise SolutionNotFoundException(2021, 1, 2)
