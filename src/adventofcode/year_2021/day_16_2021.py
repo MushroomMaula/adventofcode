37a52240096ec8e8c607edbb2c140fb9e1c3711f
Max Rausch-Dupont <maxrd79@gmail.com>
2021-12-16 21:11:11 +0100

    Add day 16

---
+++ src/adventofcode/year_2021/day_16_2021.py
@@ -0,0 +1,177 @@
+from collections import deque
+import enum
+from typing import List, Optional, Literal
+
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.helpers import solution_timer
+from adventofcode.util.input_helpers import get_input_for_day
+
+
+class Tokens(enum.Enum):
+   LITERAL = '100'
+   SUM = '000'
+   PRODUCT = '001'
+   MIN = '010'
+   MAX = '011'
+   GREATER_THAN = '101'
+   LESS_THAN = '110'
+   EQUAL = '111'
+
+
+class Packet:
+
+    @staticmethod
+    def to_binary(n: int, padding: int = 4):
+        return bin(n)[2:].zfill(padding)
+
+    @classmethod
+    def from_hex(cls, values: str):
+        content = []
+        for char in values:
+            content.append(int(char, 16))
+
+        return cls(''.join(Packet.to_binary(x) for x in content))
+
+    def __init__(self, content: str):
+        self.position = 0
+        self.content = content
+        self._versions = []
+        self._finished = False
+        self.read = 0
+
+    @property
+    def finished(self):
+        return self._finished
+
+    def version_sum(self) -> int:
+        return sum(map(lambda x: int(x, 2), self._versions))
+
+    def set_position(self, pos: int):
+        self.position = pos
+
+    def rest(self):
+        return self.content[self.position:]
+
+    def eat(self, n: int) -> str:
+        values = self.content[self.position: self.position + n]
+        self.position += n
+        self.read += n
+        return values
+
+    def parse(self) -> int:
+        version = self.eat(3)
+        self._versions.append(version)
+        type_id = self.eat(3)
+        if type_id == Tokens.LITERAL.value:
+            value = self._parse_literal()
+        else:
+            values = self._parse_operator()
+
+            if type_id == Tokens.SUM.value:
+                value = sum(values)
+
+            elif type_id == Tokens.PRODUCT.value:
+                value = 1
+                for val in values:
+                    value *= val
+
+            elif type_id == Tokens.MIN.value:
+                value = min(values)
+
+            elif type_id == Tokens.MAX.value:
+                value = max(values)
+
+            elif type_id == Tokens.GREATER_THAN.value:
+                assert len(values) == 2
+                value = values[0] > values[1]
+
+            elif type_id == Tokens.LESS_THAN.value:
+                assert len(values) == 2
+                value = values[0] < values[1]
+            elif type_id == Tokens.EQUAL.value:
+                assert len(values) == 2
+                value = values[0] == values[1]
+
+        self._finished = True
+        return value
+
+    def _parse_literal(self) -> int:
+        literals = []
+        # Take chunks of fives
+        for _ in range(len(self.rest()) // 5):
+            literal = self.eat(5)
+            # we ignore the first bit
+            _id = literal[0]
+            literals.append(literal[1:])
+
+            # This was the last literal value in the package
+            if _id == '0':
+                break
+
+        return int(''.join(literals), 2)
+
+    def _parse_operator(self) -> List[int]:
+        length_type_id = self.eat(1)
+        if length_type_id == '0':
+            bits_in_subpackets = self.eat(15)
+            n = int(bits_in_subpackets, 2)
+            value = self._parse_subpackets_by_bits(n)
+        elif length_type_id == '1':
+            num_of_subpackets = self.eat(11)
+            n = int(num_of_subpackets, 2)
+            value = self._parse_subpackets(n)
+
+        return value
+
+    def _parse_subpackets_by_bits(self, n) -> List[int]:
+        results = []
+        size = self.read + n
+        while self.read != size:
+            results.append(self.parse())
+
+        return results
+
+    def _parse_subpackets(self, n) -> List[int]:
+        results = []
+        for _ in range(n):
+            results.append(self.parse())
+
+        return results
+
+    def __repr__(self) -> str:
+        text = ''
+        for i, x in enumerate(self.content):
+            if i == self.position:
+                text += f"[{x}]"
+            else:
+                text += x
+        return text
+
+
+@solution_timer(2021, 16, 1)
+def part_one(input_data: List[str]):
+    p = Packet.from_hex(input_data[0])
+    p.parse()
+    answer = p.version_sum()
+
+    if not answer:
+        raise SolutionNotFoundException(2021, 16, 1)
+
+    return answer
+
+
+@solution_timer(2021, 16, 2)
+def part_two(input_data: List[str]):
+    p = Packet.from_hex(input_data[0])
+    answer = p.parse()
+
+    if not answer:
+        raise SolutionNotFoundException(2021, 16, 2)
+
+    return answer
+
+
+if __name__ == '__main__':
+    data = get_input_for_day(2021, 16)
+    part_one(data)
+    part_two(data)
